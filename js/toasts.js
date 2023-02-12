80ff1591515c2104e5ff607a345e134b6db3bc1a
stakancheck <stakancheck@gmail.com>
2023-02-13 02:56:27 +0300

    feat(index.html, toasts.js): add copy email action

---
+++ js/toasts.js
@@ -0,0 +1,33 @@
+const MARGIN = 10;
+let shownToasts = [];
+let toastHeight = window.innerHeight / 12;
+
+function showToast(content, time, extraClass) {
+    const toast = document.createElement("div");
+    toast.innerHTML = content;
+    toast.classList.add("toast", extraClass);
+    toast.style.bottom = toastHeight + "px";
+    document.getElementsByTagName("body")[0].appendChild(toast);
+    shownToasts.push(toast);
+    const { height } = toast.getBoundingClientRect();
+    toastHeight += MARGIN + height;
+    setTimeout(() => hideToast(toast), time);
+}
+
+function hideToast(toast) {
+    shownToasts = shownToasts.filter(e => e !== toast);
+    const { height } = toast.getBoundingClientRect();
+    toastHeight -= MARGIN + height;
+    document.getElementsByTagName("body")[0].removeChild(toast);
+    adjustPositioning();
+}
+
+function adjustPositioning() {
+    toastHeight = window.innerHeight / 12;
+    for (let i = 0; i < shownToasts.length; i++) {
+        const toast = shownToasts[i];
+        toast.style.bottom = toastHeight + "px";
+        const { height } = toast.getBoundingClientRect();
+        toastHeight += MARGIN + height;
+    }
+}
