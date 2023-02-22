6d3accdc0d2aedf467feead895802457ec34751d
stakancheck <stakancheck@gmail.com>
2023-02-22 22:01:08 +0300

    docs(all): add license

--- js/boxy-view.js
+++ js/boxy-view.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2023. Artem Sukhanov
+ *
+ * This is open source software. But copying, distribution, modification, use for commercial and personal purposes is prohibited by the author of this software. All other questions can be asked by mail: stakancheck@gmail.com
+ */
+
 window.addEventListener('load', init, false)
 
 function init() {
